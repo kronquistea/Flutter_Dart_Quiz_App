25969f85c2d616ff0346f1ab0249b20d2a414092
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-01 18:36:50 -0600

    End Lecture 55 - Also preserve trailing commas formatting style

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer.purple(),
+        body: StartScreen(),
       ),
     ),
   );
