7d5f6c34300ef74d51b66edc6c43cf3d51ff0727
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-04 21:31:16 -0600

    End Lecture 69

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,31 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 4'),
+          ),
+        ],
+      ),
+    );
   }
 }
